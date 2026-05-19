f610924a7c66a9591c4435c5037a2fbd06877490
NitinPrakash2 <codernitin268@gmail.com>
2026-05-19 20:28:12 +0530

    initial

---
+++ frontend/app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Nuxt + FastAPI</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('/api/hello')
+const message = computed(() => data.value?.message ?? 'Loading...')
+</script>
